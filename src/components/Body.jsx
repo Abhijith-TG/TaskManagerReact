06bce99212010a0796f5e5f4e512203b02d23e37
Abhijith-TG <abhijithtg62@gmail.com>
2024-11-01 18:32:01 +0530

    Task Manager Body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Body() {
   return (
-    <div>
-        
+    <div className=' h-screen w-screen bg-slate-900' >
+      <div className='flex justify-center pt-14 gap-5' >
+        <input type="text" className='text-xl outline-none ps-2 rounded' />
+        <button className=" bg-purple-600 py-2 px-4 hover:opacity-80  rounded text-white" >Add</button>
+      </div>
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className=' w-screen h-24 bg-slate-600' >
-        <div className='flex justify-center h-full items-center flex-col text-white ' >
+    <div className=' w-screen h-24 bg-gray-300' >
+        <div className='flex justify-center h-full items-center flex-col text-slate-900 ' >
             <p>Task Manager</p>
             <p>No Copyright</p>
         </div>
